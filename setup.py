ab4cd3c2d93ce8ee5afdb0a6ae135d453488599f
Same1lo <samson08092000@gmail.com>
2024-01-12 19:55:17 +0100

    oauth token now needs to be in an .env file

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 import socket
 import time
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
 
 server = "irc.chat.twitch.tv"
 port = 6667
 channel = "#castcrafter"
 bot_username = "samelloBot"
 
-oauth_token = "oauth:0"
 
 irc = socket.socket()
 irc.connect((server, port))
 
-irc.send(f"PASS {oauth_token}\r\n".encode("utf-8"))
+irc.send(f"PASS {os.getenv("oauth_token")}\r\n".encode("utf-8"))
 irc.send(f"NICK {bot_username}\r\n".encode("utf-8"))
 irc.send(f"JOIN {channel}\r\n".encode("utf-8"))
-
