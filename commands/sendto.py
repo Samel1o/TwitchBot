1ecd613234b60cc7be3b94d85196d303823d90a1
Treht <jankloss04@gmail.com>
2024-01-13 14:08:47 +0100

    Try Getting Updated

--- commands/sendto.py
+++ commands/sendto.py
@@ -1,9 +1,6 @@
 import setup
 
 def send_command(data):
-<<<<<<< HEAD
-    setup.sendMSG(setup.channel, f"{data}")
-=======
     setup.sendMSG(setup.channel, f"{data}")
 
     start_index = data.find(">sendto") + len(">sendto") + 1
@@ -15,4 +12,3 @@ def send_command(data):
     setup.sendMSG(setup.channel, f"{argList}")
 
     setup.sendMSG(f"#{argList[0]}", argList[1])
->>>>>>> 331cde5ee36898cea8288aa9c22d8ee2d11b392f
